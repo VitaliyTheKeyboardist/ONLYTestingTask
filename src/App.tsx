360f7212f95f632be3bea810f8e7228c6c3aeab5
VitaliyTheKeyboardist <vital10@inbox.ru>
2024-10-12 21:55:48 +0300

    init: removed the duplicate element

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import MainPage from "./pages/mainPage/mainPage"
 import Dates from "./assets/mocks/dates.json"
-import Dates2 from "./assets/mocks/dates2.json"
+// import Dates2 from "./assets/mocks/dates2.json"
 
 function App() {
   return (
     <div className="App">
       <MainPage content={Dates} />
-      <MainPage content={Dates2} />
+      {/* Весь компонент можно использовать несколько раз на странице, с различным набором данных, передавая их в props. 
+          Они будут децствовать независимо*/}
+      {/* <MainPage content={Dates2} /> */}
     </div>
   )
 }
